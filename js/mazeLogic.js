287e4f9c703edb19e8132fb994dc81fc95e1261c
Am4teur <daniel.7c.n12@gmail.com>
2020-07-01 12:32:44 +0100

    Merged the two Mazer projects with the updated features

--- js/mazeLogic.js
+++ js/mazeLogic.js
@@ -3,24 +3,65 @@ let ctx;
 
 /* Customized variables */
 let cellSize = 20;
-let mazeHeight = 10;
-let mazeWidth = 10;
-/* ____________________ */
-let x1 = cellSize/10;
-let y1 = cellSize/10;
-let x2 = cellSize - (cellSize/10)*2;
-let y2 = cellSize - (cellSize/10)*2;
+let mazeHeight = 8;
+let mazeWidth = 8;
 
+let setpTime = 100;
+/* ____________________ */
 let oppx = {"N": 0, "E": 1, "S": 0, "W": -1};
 let oppy = {"N": -1, "E": 0, "S": 1, "W": 0};
 let opp = {"N": "S", "E": "W", "S": "N", "W": "E"};
-/* using the x,y major order (or Column major order) != from memory and C (Row major order) */
-let positions = [...Array(mazeWidth)].map(e => Array(mazeHeight).fill(0));
-let set = [...Array(mazeWidth)].map(e => Array(mazeHeight).fill(0));
-let edges = new Array();
 
 let maze; //do not like this
 let icon; //do not like this
+
+
+let buttonPressed = false;
+let finished = false;
+let isFirstLoop = true;
+let nextTime;
+let vec = new Array();
+let count = 0;
+
+
+function Timer(time){
+
+    if(!finished) {
+        requestAnimationFrame(Timer);
+    }
+
+    if(isFirstLoop){
+        isFirstLoop=false;
+        nextTime = time;
+    }
+    
+    let elapsedTime = time - nextTime;
+    if(elapsedTime > setpTime) {
+        nextTime = time;
+        maze.delWall(vec[count][0], vec[count][1], vec[count][2]);
+        count++;
+        if(count >= vec.length) {
+            finished = true;
+            count = 0;
+        }
+    }
+}
+
+
+function stepByStep() {
+    buttonPressed = !buttonPressed;
+    finished = false;
+    count = 0;
+    SetupCanvas();
+}
+
+function redraw() {
+    finished = false;
+    count = 0;
+    SetupCanvas();
+}
+
+
 
 
 
@@ -33,121 +74,145 @@ function SetupCanvas() { //main
     canvas.height = cellSize * mazeHeight;
     canvas.width = cellSize * mazeWidth;
 
-    /*
     maze = new Maze(mazeHeight, mazeWidth);
     maze.init();
-    icon = new Icon();
+    icon = new Icon(maze);
     icon.init();
-    */
-
-    initPos();
-    initSet();
-    initEdges();
-
-    maze = new Maze(mazeHeight, mazeWidth);
-    maze.generate();
-    icon = new Icon();
-    icon.drawIcon();
 }
 
 /*_____________________ Classes _______________________*/
 class Maze {
     constructor(height, width) {
-        this.height = height;
-        this.width = width;
+        this.maze_height = height;
+        this.maze_width = width;
         
-        /* TODO */
-        this.positionsx;
-        this.setx;
-        this.edgesx;
+        /* init var positions */
+        /* using the x,y major order (or Column major order) != from memory and C (Row major order) */
+        this.positions = [...Array(this.maze_width)].map(e => Array(this.maze_height));
+        this.initPositions();
+
+        /* init var set */
+        /* put this variable set inside of kruskal algorithm method because thats the only place that this variable is used */
+        this.set = [...Array(mazeWidth)].map(e => Array(mazeHeight));
+        this.initSet();
+
+        /* init var edges */
+        this.edges = new Array();
+        this.initEdges();
     }
 
-    generate() {
+    init() {
+        vec = [];
+        this.drawGrid();
         this.kruskalAlgorithm();
-        for(let i = 0; i < positions.length; ++i) {
-            for(let j = 0; j < positions[i].length; ++j) {
-                if(positions[i][j]["N"] == 0) {
-                    this.drawWall(i, j, "N");
-                }
-                if(positions[i][j]["E"] == 0) {
-                    this.drawWall(i, j, "E");
-                }
-                if(positions[i][j]["S"] == 0) {
-                    this.drawWall(i, j, "S");
-                }
-                if(positions[i][j]["W"] == 0) {
-                    this.drawWall(i, j, "W");
-                }
-            }
+        if(buttonPressed) {
+            console.log("xxx")
+            requestAnimationFrame(Timer);
+        }
+        else {
+            this.drawMaze();
+        }
+
+    }
+
+    drawGrid() { //recheckar bem as coordenadas, deu bem logo a primeira, estranho...
+        ctx.fillStyle = "black";
+        for(let i = 0; i < this.positions.length+1; ++i) {
+            ctx.fillRect(cellSize*i  -2, 0, 4, cellSize*this.positions.length);
+        }
+        for(let i = 0; i < this.positions[0].length+1; ++i) {
+            ctx.fillRect(0, cellSize*i -2, cellSize*this.positions[0].length, 4);
         }
     }
 
-    clean() {
-        for(let i = 0; i < this.height; ++i) {
-
-        }
-        for(let i = 0; i < this.width; ++i) {
-
+    drawMaze(){
+        for(let i = 0; i < vec.length ; ++i) {
+            maze.delWall(vec[i][0], vec[i][1], vec[i][2]);
         }
     }
 
-    drawWall(x, y, side) {
-        ctx.beginPath();
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = "3";
-        switch(side) {
+    delWall(x, y, side) {
+        ctx.fillStyle = "white";
+        switch(side) { // +2 and -2 is just adjusts, nothing to do with the grid
             case "N":
-                ctx.moveTo(cellSize*x -1, cellSize*y);
-                ctx.lineTo(cellSize*(x+1), cellSize*y);
+                ctx.fillRect(cellSize*x  +2, cellSize*y  -2, cellSize  -4, 4);
                 break;
             case "E":
-                ctx.moveTo(cellSize*(x+1), cellSize*y-1);
-                ctx.lineTo(cellSize*(x+1), cellSize*(y+1));
+                ctx.fillRect(cellSize*(x+1)  -2, cellSize*y  +2, 4, cellSize  -4);
                 break;
             case "S":
-                ctx.moveTo(cellSize*x, cellSize*(y+1));
-                ctx.lineTo(cellSize*(x+1) +1, cellSize*(y+1));
+                ctx.fillRect(cellSize*x  +2, cellSize*(y+1)  -2, cellSize  -4, 4);
                 break;
             case "W":
-                ctx.moveTo(cellSize*x, cellSize*y);
-                ctx.lineTo(cellSize*x, cellSize*(y+1) +1);
+                ctx.fillRect(cellSize*x  -2, cellSize*y  +2, 4, cellSize  -4);
                 break;
             default:
-                console.log("HOW?")
+                console.log("HOW!?");
                 break;
         }
-        ctx.stroke();
     }
 
     kruskalAlgorithm() {
         let x, y, ox, oy, set1, set2;
-        for(let i = 0; i < edges.length; ++i) {
-            x = edges[i].x;
-            y = edges[i].y;
-            ox = edges[i].x + oppx[edges[i].dir];
-            oy = edges[i].y + oppy[edges[i].dir];
+        for(let i = 0; i < this.edges.length; ++i) {
+            x = this.edges[i].x;
+            y = this.edges[i].y;
+            ox = this.edges[i].x + oppx[this.edges[i].dir];
+            oy = this.edges[i].y + oppy[this.edges[i].dir];
     
-            set1 = set[x][y];
-            set2 = set[ox][oy];
+            set1 = this.set[x][y];
+            set2 = this.set[ox][oy];
             if(!(set1.connected(set2))) {
                 set1.connect(set2);
-                positions[x][y][edges[i].dir] = 1;
-                positions[ox][oy][opp[edges[i].dir]] = 1;
+                this.positions[x][y][this.edges[i].dir] = 1;
+                this.positions[ox][oy][opp[this.edges[i].dir]] = 1;
+
+                vec.push([x, y, this.edges[i].dir]);
             }
         }
     }
 
-    checkWinner() {
-        if(icon.x == maze.width-1 && icon.y == maze.height-1) {
+    async checkWinner() {
+        if(icon.x == this.positions.length-1 && icon.y == this.positions[0].length-1) {
             document.getElementById("win").innerHTML = "🎉 Congratulations Mazer, you Won! 🎉";
+            await new Promise(r => setTimeout(r, 3000)); // sleep for 3s => 3000ms
+            document.getElementById("win").innerHTML = "";
             // pop up message: "winner"
             // delay
             // automatically closes
             /* reset */
             SetupCanvas();
         }
-        else
-            document.getElementById("win").innerHTML = "";
+    }
+
+    initPositions() {
+        for(let i = 0; i < this.positions.length; ++i) {
+            for(let j = 0; j < this.positions[i].length; ++j) {
+                this.positions[i][j] = {"N": 0, "E": 0, "S": 0, "W": 0};
+            }
+        }
+    }
+
+    initSet() {
+        for(var i = 0; i < this.set.length; ++i) {
+            for(var j = 0; j < this.set[i].length; ++j) {
+                this.set[i][j] = new Tree;
+            }
+        }
+    }
+
+    initEdges() {
+        for(var i = 0; i < this.positions.length; ++i) {
+            for(let j = 0; j < this.positions[i].length; ++j) {
+                if(j>0) {
+                    this.edges.push(new Edge(i, j, "N"));
+                }
+                if(i>0) {
+                    this.edges.push(new Edge(i, j, "W"));
+                }
+            }
+        }
+        this.edges.shuffle();
     }
 }
 
@@ -186,71 +251,48 @@ class Edge {
 
 
 class Icon {
-    constructor() {
+    constructor(myMaze) {
         this.x = 0;
         this.y = 0;
+        this.myMaze = myMaze;
+
+        this.x1 = cellSize/10;
+        this.y1 = cellSize/10;
+        this.x2 = cellSize - (cellSize/10)*2;
+        this.y2 = cellSize - (cellSize/10)*2;
+    }
+
+    init() {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.x1+cellSize*this.x, this.y1+cellSize*this.y, this.x2, this.y2);
+
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x1, this.y1+cellSize, this.x2, this.y2);
     }
 
     moveIcon() {
-        ctx.fillStyle = "black";
-        ctx.fillRect(x1, y1, x2, y2);
-    }
-
-    drawIcon() {
-        ctx.fillStyle = "black";
-        ctx.fillRect(x1+cellSize*this.x, y1+cellSize*this.y, x2, y2);
-
         ctx.fillStyle = "blue";
-        ctx.fillRect(x1, y1+cellSize, x2, y2);
+        ctx.fillRect(this.x1, this.y1, this.x2, this.y2);
     }
 
     deleteIcon() {
         ctx.fillStyle = "white";
-        ctx.fillRect(x1+cellSize*this.x, y1+cellSize*this.y, x2, y2);
+        ctx.fillRect(this.x1+cellSize*this.x, this.y1+cellSize*this.y, this.x2, this.y2);
     }
 
     move(dir){
-        if(positions[this.x][this.y][dir]) {
+        if(this.myMaze.positions[this.x][this.y][dir]) {
             this.deleteIcon();
             this.x += oppx[dir];
             this.y += oppy[dir];
-            this.drawIcon();
-            maze.checkWinner();
+            this.init(); //check this
+            this.myMaze.checkWinner();
+            finished = false;
         }
     }
 }
 
 /*_____________________ Functions _______________________*/
-function initPos() {
-    for(var i = 0; i < positions.length; ++i) {
-        for(var j = 0; j < positions[i].length; ++j) {
-            positions[i][j] = {"N": 0, "E": 0, "S": 0, "W": 0};
-        }
-    }
-}
-
-function initSet() {
-    for(var i = 0; i < set.length; ++i) {
-        for(var j = 0; j < set[i].length; ++j) {
-            set[i][j] = new Tree;
-        }
-    }
-}
-
-function initEdges() {
-    for(var i = 0; i < positions.length; ++i) {
-        for(let j = 0; j < positions[i].length; ++j) {
-            if(j>0) {
-                edges.push(new Edge(i, j, "N"));
-            }
-            if(i>0) {
-                edges.push(new Edge(i, j, "W"));
-            }
-        }
-    }
-    edges.shuffle();
-}
-
 Array.prototype.shuffle = function() {
     let m = this.length;
     let i;
@@ -264,24 +306,20 @@ Array.prototype.shuffle = function() {
 
 function keyHandler(e) {
     switch (e.keyCode) {
-        case 37:
-            //icon.moveLeft();
+        case 37: //left
             icon.move("W");
             break;
-        case 38:
-            //icon.moveUp();
+        case 38: //up
             icon.move("N");
             break;
-        case 39:
-            //icon.moveRight();
+        case 39: //right
             icon.move("E");
             break;
-        case 40:
-            //icon.moveDown();
+        case 40: //down
             icon.move("S");
             break;
         default:
-            console.log("No function for that key");
+            //console.log("No function for that key");
             break;
     }
 };
